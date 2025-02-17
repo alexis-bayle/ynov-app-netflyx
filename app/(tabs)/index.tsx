00018a2029c29a7c9c838b7275c6c61a4818de92
alexis <alexisbayle@hotmail.com>
2025-02-17 15:43:03 +0100

    background gradient test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,21 +4,27 @@ import { StyleSheet, View } from 'react-native';
 
 import { ScreenContent } from '~/components/ScreenContent';
 import {useEffect} from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Home() {
-    useEffect(() => {
-
-        NavigationBar.setVisibilityAsync('hidden');
-        // Pour un mode immersif complet (ex: swipes pour revenir)
-        NavigationBar.setBehaviorAsync('overlay-swipe');
-    }, []);
+  useEffect(() => {
+    NavigationBar.setVisibilityAsync('hidden');
+    NavigationBar.setBehaviorAsync('overlay-swipe');
+  }, []);
 
   return (
     <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Home" />
-      </View>
+      <Stack.Screen options={{ title: 'Home', headerShown: false }} />
+      <LinearGradient
+        colors={['#0B9E9F', '#0A0A0A']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        style={styles.linearGradient}>
+
+        <View style={styles.container}>
+          <ScreenContent path="app/(tabs)/index.tsx" title="Home" />
+        </View>
+      </LinearGradient>
     </>
   );
 }
@@ -27,5 +33,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
+  },
+  linearGradient: {
+    flex: 1,
   },
 });
